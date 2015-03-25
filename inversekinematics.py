cca7df0d9dc40a521ec10efe798df1c0f6f35986
Giles Paterson <giles@vurt.co.uk>
2015-03-25 19:39:14 +0000

    Modified inversekinematics to ensure non-negative values

--- inversekinematics.py
+++ inversekinematics.py
@@ -9,21 +9,25 @@ class Kinematics(object) :
     def computeAngles(self, x, y):
         x = float(x)
         y = float(y)
-        c = sqrt(x**2 + y**2)
+        c = x**2 + y**2
         angle1 = self._computeAngle1(x, y, c)
         angle2 = self._computeAngle2(x, y, c)
         return (angle1, angle2)
 
     def _computeAngle2(self, x, y, c):
-        return degrees(acos(((self.a**2) + (self.b**2) - c) / (2 * self.a * self.b)))
+        top = ((self.a**2) + (self.b**2) - c)
+        bottom = (2 * self.a * self.b)
+        return degrees(acos(top / bottom))
 
     def _computeAngle1(self, x, y, c):
-        return degrees(atan(y/x) + acos(((self.a**2) - (self.b**2)) + c / ((2 * self.a) * c)))
+        rads = atan(y/x) + acos(abs(((self.a**2) - (self.b**2)) + c) / ((2 * self.a) * c))
+        return degrees(rads)
+
 
 if __name__ == "__main__":
-    print "X = 10"
-    print "Y = 10"
-    k = Kinematics(5, 5)
-    (angle1, angle2) = k.computeAngles(10, 10)
+    print "X = 200"
+    print "Y = 100"
+    k = Kinematics(247, 285)
+    (angle1, angle2) = k.computeAngles(200, 100)
     print "angle1 = " + str(angle1)
     print "angle2 = " + str(angle2)
--- test1.py
+++ test1.py
@@ -4,13 +4,19 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 motor1 = Motor([7,8,9,10])
-motor2 = Motor([20,21,22,23])
-motor1.rpm = 16
-motor2.rpm = 16
+motor2 = Motor([17,18,22,23])
+motor1.rpm = 5
+motor2.rpm = 5
 
-kin = Kinematics(100, 100) #millimetres
+kin = Kinematics(247, 285) #millimetres
 
-(angle1, angle2) = kin.computeAngles(50, 50)
+(angle1, angle2) = kin.computeAngles(200, 100)
 
-motor1.moveTo(angle1)
-motor2.moveTo(180 - angle2)
+print "Moving 1 to " + str(angle1)
+motor1.moveTo(-angle1)
+print "Moving 2 to " + str(angle2)
+#motor2.moveTo(180 - angle2)
+motor2.moveTo(-angle2)
+motor1.release()
+motor2.release()
+GPIO.cleanup()
