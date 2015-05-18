7e31445e0d73ce1d8fc3a8424b79b1decb29bd36
Giles Paterson <giles.paterson@blackpepper.co.uk>
2015-05-18 18:54:46 +0100

    Playing with computeAngle parameters

--- dualkinematics.py
+++ dualkinematics.py
@@ -1,5 +1,5 @@
 from math import acos, atan, asin, sin, degrees, sqrt
-from pythagoras import cosineRule
+from pythagoras import cosineRule, angleA
 
 class Kinematics(object) :
 
@@ -15,15 +15,23 @@ class Kinematics(object) :
 
     def computeAngle(self, x, y, l1, l2):
         c = sqrt(x**2 + y**2)
-        e = cosineRule(l1, l2, c)
+        e = angleA(c, l2, l1)
         a = atan(y/x)
-        b = asin((sin(e)/c) * l1)
+        b = angleA(l2, l1, c)
         return a+b
 
+#    def computeAngle(self, x, y, l1, l2):
+#        c = sqrt(x**2 + y**2)
+#        e = cosineRule(l1, l2, c)
+#        a = atan(y/x)
+#        b = asin((sin(e)/c) * l1)
+#        return a+b
+
     def computeAngles(self, x, y):
-        motor1 = self.computeAngle(x, -(self.m1y + y), self.l1, self.l2)
+        offsetX = x - self.m1x
+        motor1 = self.computeAngle(offsetX, y + (-self.m1y), self.l1, self.l2)
         diff = self.m2x - self.m1x
-        motor2 = self.computeAngle(diff - x, -(self.m2y + y), self.r1, self.r2)
+        motor2 = self.computeAngle(diff - offsetX, y + (-self.m2y), self.r1, self.r2)
         return (degrees(motor1), degrees(motor2))
 
     # Based on this diagram:
@@ -36,3 +44,5 @@ if __name__ == "__main__":
     (angle1, angle2) = k.computeAngles(200, 100)
     print "angle1 = " + str(angle1)
     print "angle2 = " + str(angle2)
+    print "offset angle1 = " + str(90 - angle1)
+    print "offset angle2 = " + str(90 - angle2)
